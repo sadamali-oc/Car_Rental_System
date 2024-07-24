4f7d1b82eaeda600d7b9a0b9e79ed5c5fb9dc715
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-07-24 13:57:52 +0530

    Admin and Client classes are created

--- Admin.java
+++ Admin.java
@@ -1,2 +1,20 @@
-public class Admin {
+public class Admin extends  User {
+    public Admin() {
+        super();
+    }
+
+    @Override
+    public void showList() {
+
+        System.out.println("\n1. Add New Car");
+        System.out.println("\n2. View Cars");
+        System.out.println("\n3. Update Car");
+        System.out.println("\n4. Delete Car");
+        System.out.println("\n5. Add New Admin");
+        System.out.println("\n6. Show Rents");
+        System.out.println("\n7. Quit\n");
+
+
+
+    }
 }
---
+++ Client.java
@@ -0,0 +1,18 @@
+public class Client extends  User {
+
+    public Client() {
+        super();
+    }
+
+    @Override
+    public void showList() {
+        System.out.println("\n1. View Car");
+        System.out.println("\n2. Rent Cars");
+        System.out.println("\n3. Return Car");
+        System.out.println("\n4. Show My Rents");
+        System.out.println("\n5. Edit My Data");
+        System.out.println("\n7. Quit\n");
+
+
+    }
+}
